0c007f94673797828638864a3fb137214a5c7b43
Dinkar <dinkar.rachapudi@gmail.com>
2015-07-12 00:39:44 -0400

    Batch support for high volume data

--- java/SpringApplication/TreeFinder/src/com/dinkarrachapudi/soatreefinder/OracleDBReader.java
+++ java/SpringApplication/TreeFinder/src/com/dinkarrachapudi/soatreefinder/OracleDBReader.java
@@ -16,6 +16,9 @@ public class OracleDBReader implements IReader{
 	   RowCallbackHandler rcbHandler;
 	   private String filePath;
 	   private FileWriter fileWriter;
+	   private int recordsPerBatch;
+	   private String countSQL;
+	   private String columnsSQL;
 	   
 	   public void setDataSource(DataSource dataSource) {
 		      this.dataSource = dataSource;
@@ -24,20 +27,31 @@ public class OracleDBReader implements IReader{
 	   
 	   public void setFilePath(String filePath) {
 		      this.filePath = filePath;
-		      try{
-		    	  this.fileWriter = new FileWriter(filePath,false);
-		    	  this.rcbHandler = new CSVWriter(fileWriter);
-				   }
-				   catch(IOException e){
-					   
-				   }
 		      System.out.println("Writing to " + filePath);
 		   }
+	   
+	   public void setRecordsPerBatch(int recordsPerBatch){
+		   this.recordsPerBatch = recordsPerBatch;
+	   }
+	   
+	   public void setCountSQL(String countSQL){
+		   this.countSQL = countSQL;
+	   }
+	   
+	   public void setColumnsSQL(String columnsSQL){
+		   this.columnsSQL = columnsSQL;
+	   }
+	  
 
 	   @Override
 	public void fetchData(String SQL){
+		   int totalRecords = jdbcTemplateObject.queryForObject(countSQL, Integer.class);
+		   System.out.println("Total records are " + totalRecords);
+		   int startRow = 0;
+		   int endRow = recordsPerBatch;
 		   try{
-		   SqlRowSetMetaData cubeCompositeMetadata = jdbcTemplateObject.queryForRowSet(SQL).getMetaData();
+		   SqlRowSetMetaData cubeCompositeMetadata = jdbcTemplateObject.queryForRowSet(columnsSQL).getMetaData();
+		   FileWriter fileWriter = new FileWriter(filePath,true);
 		   for(int i=1;i<=cubeCompositeMetadata.getColumnCount();i++){
 			   fileWriter.append(cubeCompositeMetadata.getColumnName(i));
 			   if(i==cubeCompositeMetadata.getColumnCount())
@@ -46,13 +60,23 @@ public class OracleDBReader implements IReader{
 				   fileWriter.append(",");
 			   }
 		   }
+		   fileWriter.flush();
+		   fileWriter.close();
+		   
+		   
+		   while(totalRecords>startRow){
+			   System.out.println("Executing SQL " + SQL + " where rn>" + startRow + " and rn<=" + endRow);
+			   fileWriter = new FileWriter(filePath,true);
+			   rcbHandler = new CSVWriter(fileWriter);
+			   jdbcTemplateObject.query(SQL + " where rn>" + startRow + " and rn<=" + endRow, rcbHandler);
+			   System.out.println("Completed CSVWrite for SQL " + SQL + " where rn>" + startRow + " and rn<=" + endRow);
+			   startRow = startRow + recordsPerBatch;
+			   endRow = startRow + recordsPerBatch;
+		   }
 		   }
 		   catch(IOException e) {
 			   System.out.println("IOException: " + e);
 		   }
-		   System.out.println("Executing SQL " + SQL);
-	      jdbcTemplateObject.query(SQL,rcbHandler);
-	      
 	}
 	   
 	   public void closeReader(){
